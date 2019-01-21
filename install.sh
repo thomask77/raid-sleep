d737b3118159e237965b1d8232129a0107955bd1
Andre Heider <a.heider@gmail.com>
2019-01-21 09:38:51 +0100

    install.sh: use explicit systemctl parameters, skip pager

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ install -m 644 -v raid-sleep.service /etc/systemd/system
 test -f /etc/raid-sleep.conf || install -m 644 -v raid-sleep.conf /etc/raid-sleep.conf
 
 systemctl daemon-reload
-systemctl enable raid-sleep
-systemctl restart raid-sleep
-systemctl status raid-sleep -l
+systemctl enable raid-sleep.service
+systemctl restart raid-sleep.service
+systemctl status raid-sleep.service --no-pager -l
